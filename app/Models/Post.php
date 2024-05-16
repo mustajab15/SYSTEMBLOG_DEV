d596a3b94a9afbafc0aec61dd921c466ae76ea82
mustajab15 <mustajabamir151@gmail.com>
2024-05-16 09:38:31 +0800

    ORM & Models Commit

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,43 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post
+class Post extends Model
 {
-    public static function all()
-    {
-        return  [
-            [
-                'id' => 1,
-                'slug' => 'judul-artikel-1',
-                'title' => 'judul artikel 1',
-                'author' => 'Mustajab',
-                'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos aut consequatur sit cumque nemo optio totam provident suscipit officiis dicta aperiam, sequi dolor architecto quos dolorem ipsam debitis nobis. Rerum!'
-            ],
-            [
-                'id' => 2,
-                'slug' => 'judul-artikel-2',
-                'title' => 'judul artikel 2',
-                'author' => 'Mustajab',
-                'body' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio tempore, laudantium eius, rem magni quidem temporibus, libero consequatur officiis deserunt consectetur molestias commodi quam minima obcaecati corporis quis autem velit.'
-            ]
-        ];
-    }
-
-    public static function find($slug): array
-    {
-    $post =  Arr::first(static::all() , fn($post) => $post['slug'] == $slug);
-
-    if(!$post){
-        abort(404);
-    }
-
-    return $post;
-
-    }
-
+    use HasFactory;
+    protected $fillable = ['title','author','slug','body'];
 }
-
-
-?>
